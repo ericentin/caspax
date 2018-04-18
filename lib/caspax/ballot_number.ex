b5b9712cf8e3197553a38875334b9151b2bc4bbe
Aaron Seigo <aaron@nomoko.world>
2018-04-18 10:50:46 +0200

    use __MODULE__ and catch errors from repeated calls to init

--- lib/caspax/ballot_number.ex
+++ lib/caspax/ballot_number.ex
@@ -1,29 +1,35 @@
 defmodule Caspax.BallotNumber do
   def init do
-    :ets.new(Caspax.BallotNumber, [
-      :public,
-      :set,
-      :named_table,
-      {:read_concurrency, true},
-      {:write_concurrency, true}
-    ])
+    try do
+      :ets.new(__MODULE__, [
+        :public,
+        :set,
+        :named_table,
+        {:read_concurrency, true},
+        {:write_concurrency, true}
+      ])
+    rescue
+      # the ets table already exists
+      # so we return the name of the table as :ets.new/2 does
+      ArgumentError -> __MODULE__
+    end
   end
 
   def get_next do
     :ets.update_counter(
-      Caspax.BallotNumber,
-      Caspax.BallotNumber,
+      __MODULE__,
+      __MODULE__,
       1,
-      {Caspax.BallotNumber, -1}
+      {__MODULE__, -1}
     )
   end
 
   def fast_forward(new_ballot_number) do
-    curr = :ets.lookup_element(Caspax.BallotNumber, Caspax.BallotNumber, 2)
+    curr = :ets.lookup_element(__MODULE__, __MODULE__, 2)
     diff = new_ballot_number - curr
 
     if diff > 0 do
-      :ets.update_counter(Caspax.BallotNumber, Caspax.BallotNumber, diff)
+      :ets.update_counter(__MODULE__, __MODULE__, diff)
     else
       curr
     end
