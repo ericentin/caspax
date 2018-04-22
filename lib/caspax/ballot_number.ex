2e9af3b7a188f6d55a02371174c3c4f1bdfed533
Aaron Seigo <aseigo@kde.org>
2018-04-22 22:59:32 +0200

    make ffwd of ballot numbers atomic
    uses a matchspec and select_replace to achieve this

--- lib/caspax/ballot_number.ex
+++ lib/caspax/ballot_number.ex
@@ -25,13 +25,25 @@ defmodule Caspax.BallotNumber do
   end
 
   def fast_forward(new_ballot_number) do
-    curr = :ets.lookup_element(__MODULE__, __MODULE__, 2)
-    diff = new_ballot_number - curr
+    # the following is a match spec that looks for two-tuple entries
+    # (aka a counter) where the key is our key and the value is less
+    # than what we are fast forwarding to.
+    #
+    # not pretty, but ensures an atomic update to prevent race conditions
+    # between different processes trying to change the ballot number
+    #
+    # see http://erlang.org/doc/apps/erts/match_spec.html
+    ms = [
+      {
+        {:"$1", :"$2"},
+        [{:<, :"$2", {:const, new_ballot_number}}, {:"=:=", {:const, __MODULE__}, :"$1"}],
+        [{{:"$1", {:const, new_ballot_number}}}]
+      }
+    ]
 
-    if diff > 0 do
-      :ets.update_counter(__MODULE__, __MODULE__, diff)
-    else
-      curr
+    case :ets.select_replace(__MODULE__, ms) do
+      0 -> :ets.lookup_element(__MODULE__, __MODULE__, 2)
+      _ -> new_ballot_number
     end
   end
 end
